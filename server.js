def9cd040c7fdc9287712f906ac6abfe4f9f5f39
Taranveer Singh <taranveersingh93@gmail.com>
2024-01-21 04:39:47 -0500

    Fix weekend caching

--- server.js
+++ server.js
@@ -9,34 +9,69 @@ require('dotenv').config()
 app.set('port', process.env.PORT || 3001);
 app.locals.title = 'Above Average API';
 
-app.use(cors({
-  origin: 'https://taranveer-above-average.vercel.app'
-}));
-// app.use(cors());
+// app.use(cors({
+//   origin: 'https://taranveer-above-average.vercel.app'
+// }));
+app.use(cors());
 app.use(express.json());
 
-const isDateToday = (date) => {
+const isDateToday = (cacheDate) => {
+  if (!cacheDate) {
+    return false;
+  }
   const today = new Date();
-  const dateString = today.toLocaleString("en-US", {
-    timeZone: "America/New_York",
-  });
-  const dateArray1 = dateString.split("/"); // mm/dd/yyyy
+  const todayHours = today.getUTCHours();
+  const cacheHours = cacheDate.getUTCHours();
+  const cacheTimeCutoffGood = cacheDate.getUTCHours() > 10 || (cacheDate.getUTCHours() == 10 && cacheDate.getMinutes() > 30)
+  const currentTimeCutOffGood = today.getUTCHours() > 10 || (today.getUTCHours() === 10 && today.getMinutes() > 30)
+  const weekendToday = today.getDay() === 6 || today.getDay() === 0;
 
-  const dateArray2 = date.split("-"); //yyyy-mm-dd
-  const datesMatch = parseInt(dateArray1[1]) === parseInt(dateArray2[2]);
-  const monthsMatch = parseInt(dateArray1[0]) === parseInt(dateArray2[1]);
-  const yearsMatch = parseInt(dateArray1[2]) === parseInt(dateArray2[0]);
-  return datesMatch && monthsMatch && yearsMatch;
+  //weekend today but cache is good.
+  if (weekendToday && cacheDate.getDay() === 5 && cacheTimeCutoffGood) {
+    return true
+  }
+
+  //weekend same day 
+  if (weekendToday && today.getDate() === cacheDate.getDate() && today.getMonth() === today.getMonth()) {
+    return true
+  }
+
+  //same day after 5:30
+  if (today.getDate() === cacheDate.getDate() && today.getMonth() === today.getMonth() && cacheTimeCutoffGood) {
+    return true;
+  }
+
+  //same day, before 5:30
+  if (today.getDate() === cacheDate.getDate() && today.getMonth() === today.getMonth() && !cacheTimeCutoffGood && !currentTimeCutOffGood) {
+    return true;
+  }
+
+  //different day, not more than 1 day gap, cache is past cutoff but today time not past 5:30 
+  if (today - cacheDate <= 86400000 && cacheTimeCutoffGood && !currentTimeCutOffGood) {
+    return true;
+  }
+
+  return false;
+  // const dateString = today.toLocaleString("en-US", {
+  //   timeZone: "America/New_York",
+  // });
+  // const dateArray1 = dateString.split("/"); // mm/dd/yyyy
+
+  // const dateArray2 = date.split("-"); //yyyy-mm-dd
+  // const datesMatch = parseInt(dateArray1[1]) === parseInt(dateArray2[2]);
+  // const monthsMatch = parseInt(dateArray1[0]) === parseInt(dateArray2[1]);
+  // const yearsMatch = parseInt(dateArray1[2]) === parseInt(dateArray2[0]);
+  // return datesMatch && monthsMatch && yearsMatch;
 };
 
 app.get('/nasdaqData', async (req, res) => {
   try {
 
-    const localResponse = myCache.get("nasdaqData");
+    let localResponse = myCache.get("nasdaqData");
     let localResponseGood = false;
 
     if (localResponse?.historical) {
-        localResponseGood = isDateToday(localResponse.historical[0].date);
+        localResponseGood = isDateToday(myCache.get("nasdaqDataDate"));
     }
 
     if(localResponse && localResponseGood) {
@@ -44,8 +79,10 @@ app.get('/nasdaqData', async (req, res) => {
     } else {
       response = await fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/%5ENDX?apikey=${process.env.REACT_APP_API_KEY}`)
       const responseJSON = await response.json()
+      const cacheDate = new Date();
+      myCache.set("nasdaqDataDate", cacheDate);
       myCache.set("nasdaqData", responseJSON);
-      
+
       if (response.status === 429) {
         res.status(429).json(responseJSON)
       } else if (!response.ok) {
@@ -70,6 +107,8 @@ app.get('/nasdaqConstituents', async (req, res) => {
       const response = await fetch(`https://financialmodelingprep.com/api/v3/nasdaq_constituent?apikey=${process.env.REACT_APP_API_KEY}`)
       const responseJSON = await response.json();
       myCache.set("nasdaqConstituents", responseJSON, 3600);
+      const cacheDate = new Date();
+      myCache.set("nasdaqConstituentsDate", cacheDate);
       if (response.status === 429) {
         res.status(429).json(responseJSON)
       } else if (!response.ok) {
@@ -93,16 +132,16 @@ app.get('/:symbol', async (req, res) => {
     let localResponseGood = false;
 
     if (localResponse?.historical) {
-      localResponseGood = isDateToday(localResponse.historical[0].date);
+      localResponseGood = isDateToday(myCache.get(`${symbol}StockDate`));
     }
     
-
-    if (localResponseGood) {
+    if (localResponse && localResponseGood) {
       res.status(200).json(localResponse);
     } else {
       const response = await fetch(`https://financialmodelingprep.com/api/v3/historical-price-full/${symbol}?timeseries=151&apikey=${process.env.REACT_APP_API_KEY}`)
       const responseJSON = await response.json()
-      
+      const cacheDate = new Date();
+      myCache.set(`${symbol}StockDate`, cacheDate);
       myCache.set(`${symbol}Stock`, responseJSON);
   
       if (response.status === 429) {
